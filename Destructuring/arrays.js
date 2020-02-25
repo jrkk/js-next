c158fb9f30616196ad60936493c4ffc4eb4d3e2a
kiran.jrk <kiran.jrk@abhibus.com>
2020-02-25 17:30:33 +0530

    Object and Array Destructuring

---
+++ Destructuring/arrays.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const data = [
+    { name: 'jrkkiran', age: 30 },
+    { name: 'paddu', age: 27 },
+    { name: 'isabelle', age: 2 }
+];
+
+const [ , first ] = data;
+
+console.log(first);
---
+++ Destructuring/objects.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const creatures = {
+    birds : ['dove','parrot','nail'],
+    animals : ['cow', 'dog', 'cat'],
+    wildAnimals : ['tiger', 'lion', 'hyna'],
+    seaAnimals : ['shark', 'whale', 'dolphin']
+}
+
+const { animals } = creatures;
+
+console.log(animals);
